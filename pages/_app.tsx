37bc6398d01cc10ae69d69b80f07869b305968c7
Tanaka Yuki <tnkyk0126@gmail.com>
2022-12-09 00:35:35 +0900

    ts化

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,64 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/global.css'
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+import { useMediaQuery } from '@mui/material'
+import Layout from '../components/Layout'
+import { AppProps } from 'next/app'
+import { ReactElement, ReactNode } from 'react'
+import { NextPage } from 'next'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode
 }
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  const isDarkMode = useMediaQuery('(prefers-color-scheme: dark)')
+  const theme = createTheme({
+    palette: {
+      mode: isDarkMode ? 'dark' : 'light',
+      primary: {
+        main: '#3949ab',
+        light: '#6f74dd',
+        dark: '#00227b',
+      },
+      secondary: {
+        main: '#ffa726',
+        light: '#ffd95b',
+        dark: '#c77800',
+      },
+      info: {
+        main: '#ff8a65',
+        light: '#ffbb93',
+        dark: '#c75b39',
+      },
+      success: {
+        main: '#7cb342',
+        light: '#aee571',
+        dark: '#4b830d',
+      },
+    },
+    components: {
+      MuiButtonBase: {
+        defaultProps: {
+          disableRipple: true,
+          disableTouchRipple: true,
+        },
+      },
+    },
+    typography: {
+      fontFamily: ['Roboto', 'noto-sans-serif'].join(','),
+    },
+  })
+  const getLayout = Component.getLayout || ((page: ReactElement) => page)
+
+  return (
+    <ThemeProvider theme={theme}>
+      {getLayout(<Component {...pageProps} />)}
+    </ThemeProvider>
+  )
+}
+
+export default MyApp
